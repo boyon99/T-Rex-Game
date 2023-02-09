94a04bc307054ec56468a7247a0d5486925a8d3c
boyon99 <bo_yon_99@naver.com>
2023-02-09 13:03:36 +0900

    :memo: feat: Add dino

---
+++ main.js
@@ -0,0 +1,20 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
+// dino
+let dino = {
+  x : 10,
+  y : 200,
+  width : 50,
+  height : 50,
+  draw(){
+    ctx.fillStyle = 'green';
+    ctx.fillRect(this.x,this.y,this.width, this.height);
+  }  
+}
+
+dino.draw()
+
