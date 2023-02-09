aae047b8bcdca8ef9642454c407bd204bc52958b
boyon99 <bo_yon_99@naver.com>
2023-02-09 13:29:11 +0900

    :memo: feat: Add cactus_box
    장애물 여러 개 생성하는 함수 구현

--- main.js
+++ main.js
@@ -6,31 +6,55 @@ canvas.height = window.innerHeight - 100;
 
 // 공룡
 let dino = {
-  x : 10,
-  y : 200,
-  width : 50,
-  height : 50,
-  draw(){
+  x: 10,
+  y: 200,
+  width: 50,
+  height: 50,
+  draw() {
     ctx.fillStyle = 'green';
-    ctx.fillRect(this.x,this.y,this.width, this.height);
-  }  
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 }
 
 dino.draw()
 
 // 장애물
 class Cactus {
-  constructor(){
-    this.x = 500;
+  constructor() {
+    this.x = window.innerWidth - 200;
     this.y = 200;
     this.width = 50;
     this.height = 50;
   }
-  draw(){
+  draw() {
     ctx.fillStyle = 'red';
-    ctx.fillRect(this.x,this.y,this.width, this.height);
-  } 
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 }
 
-let cactus = new Cactus();
-cactus.draw()
+
+let timer = 0;
+let cactus_box = [];
+
+// animation
+function frames() {
+  requestAnimationFrame(frames);
+  timer++;
+  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+  if(timer%200 === 0){
+    let cactus = new Cactus();
+    cactus_box.push(cactus);
+  }
+
+  cactus_box.forEach((a)=>{
+    a.x--;
+    a.draw()
+  })
+
+  dino.draw()
+}
+
+frames()
